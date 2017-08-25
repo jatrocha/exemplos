217a9b6c1fa1d7572e610646f5e11882f6257b07
Joao Rocha <jatrocha@gmail.com>
2017-08-25 15:57:57 -0300

    Atualizacao dos testes de interface

--- web/src/test/java/br/com/cygnus/exemplos/frontend/FrontendIT.java
+++ web/src/test/java/br/com/cygnus/exemplos/frontend/FrontendIT.java
@@ -1,41 +1,44 @@
 package br.com.cygnus.exemplos.frontend;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class FrontendIT {
-
-   private WebDriver browser;
 
    @Before
    public void setup() {
 
-      this.browser = new FirefoxDriver();
    }
 
    @Test
    public void startTest() {
-      this.browser.get("http://localhost:8081/examples/");
 
-      this.browser.findElement(By.id("id")).click();
+      final WebDriver browser = new HtmlUnitDriver();
 
-      // Will throw exception if elements not found
-      this.browser.findElement(By.id("id")).sendKeys("011");
+      browser.get("http://localhost:8081/examples/");
 
-      this.browser.findElement(By.id("titulo")).sendKeys("Título");
+      browser.findElement(By.id("id")).click();
 
-      this.browser.findElement(By.id("autor")).sendKeys("Autor");
+      browser.findElement(By.id("id")).sendKeys("011");
 
-      this.browser.findElement(By.id("genero")).sendKeys("Gênero");
+      browser.findElement(By.id("titulo")).sendKeys("Título");
 
-      this.browser.findElement(By.id("btnSubmit")).click();
+      browser.findElement(By.id("autor")).sendKeys("Autor");
+
+      browser.findElement(By.id("genero")).sendKeys("Gênero");
+
+      browser.findElement(By.id("btnSubmit")).click();
+
       // this.browser.findElement(By.id("account")).click();
 
       // assertEquals("John", this.browser.findElement(By.id("firstName")).getAttribute("value"));
+
+      Assert.assertTrue(true);
 
    }
 
